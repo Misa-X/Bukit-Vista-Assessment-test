37a8b898abd97dbb66f3702de432d341bd7bfbea
misaxirinda <misaxirinda@gmail.com>
2022-01-18 00:36:17 +0700

    sup

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+const movieController = require("./src/controllers/favoriteMovies_controller")
 const express = require('express'); // load express
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
@@ -21,8 +21,8 @@ app.use(express.urlencoded({extended: true}))
 app.get('/', function(req, res) {
   res.send('hello world');
 });
-//app.get('/moviesfromdb', movieController.getAllMovies)
-//app.get('/movies/imdb/:title', movieController.getMovieByTitleFromApi)
+app.get('/moviesfromdb', movieController.getAllMovies)
+app.get('/movies/imdb/:title', movieController.getMovieByTitleFromApi)
 const router = require('./src/routes/movieRouter.js')
 app.use('/api', router)
 app.use(express.json());
